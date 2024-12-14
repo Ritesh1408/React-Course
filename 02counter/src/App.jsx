ef498c9b02812169256e04b1d33da4211ef2ce65
RITESH ROUSHAN KUMAR <78962046+Ritesh1408@users.noreply.github.com>
2024-12-14 11:34:59 +0530

    First React project

---
+++ 02counter/src/App.jsx
@@ -0,0 +1,39 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  
+  let [counter, setcounter] = useState(0)
+
+  const increment = () => {
+    if(counter >= 20){
+      alert("Counter reached to 20")
+      setcounter(0)
+      return
+    }
+    setcounter(counter + 1)
+
+  }
+
+  const Decrement = () => {
+    if(counter <= 0){
+      alert("Counter reached to 0")
+      setcounter(0)
+      return
+    }
+    setcounter(counter - 1)
+  }
+
+  return (
+    <>
+      <h1>Counter App</h1>
+      <h2>{counter}</h2>
+      <button onClick={increment}>Increment</button>
+      <button onClick={Decrement}>Decrement</button>
+
+    </>
+  )
+}
+
+export default App
